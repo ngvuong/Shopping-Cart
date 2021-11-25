3d793170e00d04c61cb6910dfac02b25e0f558da
ngvuong <vminguyen@ucdavis.edu>
2021-11-25 02:31:44 -0800

    Update cart, add overlay

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default function Cart() {
+  const cartRef = useRef();
+
   return (
     <div>
-      <div className="cart-icon">Cart</div>
-      <div className="cart"></div>
+      <div className="overlay"></div>
+      <div
+        className="cart-icon"
+        onClick={() => cartRef.current.classList.add("open")}
+      >
+        Cart
+      </div>
+      <div className="cart" ref={cartRef}>
+        <span
+          className="btn-close-cart"
+          onClick={() => cartRef.current.classList.remove("open")}
+        >
+          &#10006;
+        </span>
+      </div>
     </div>
   );
 }
