0c833a3d11ca8b646d532e719030cf57f5b765de
ngvuong <vminguyen@ucdavis.edu>
2021-11-29 23:03:17 -0800

    Add product images

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,9 @@ export default function Product({ product, name, onClick }) {
   return (
     <div className="product-card">
       <div className="product">{name}</div>
+      <div className="image">
+        <img src={product.src} alt="" />
+      </div>
       <button className="btn-add-to-cart" onClick={() => onClick(product)}>
         Add to Cart
       </button>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import Cart from "./Cart";
 import Product from "./Product";
+import cart1 from "../assets/cart1.webp";
+import cart2 from "../assets/cart2.webp";
+import cart3 from "../assets/cart3.jpg";
+import cart4 from "../assets/cart4.jpg";
+import cart5 from "../assets/cart5.jpg";
+import cart6 from "../assets/cart6.webp";
 
 const products = [
-  { id: 1, name: "Product1" },
-  { id: 2, name: "Product2" },
-  { id: 3, name: "Product3" },
-  { id: 4, name: "Product4" },
-  { id: 5, name: "Product5" },
-  { id: 6, name: "Product6" },
+  { id: 1, name: "Tiny 7 Wheeler", src: cart1 },
+  { id: 2, name: "Product2", src: cart2 },
+  { id: 3, name: "Product3", src: cart3 },
+  { id: 4, name: "Product4", src: cart4 },
+  { id: 5, name: "Product5", src: cart5 },
+  { id: 6, name: "Product6", src: cart6 },
 ];
 
 export default function Shop() {
